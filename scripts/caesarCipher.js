aa1b986e973345888f809e9a04ef6e46936b8dc6
stvdeshon <stvdeshon@gmail.com>
2023-01-10 14:43:39 -0700

    Save work

--- scripts/caesarCipher.js
+++ scripts/caesarCipher.js
@@ -7,13 +7,7 @@ function caesarCipher(string, key) {
     if (string[i] === " ") {
       ciphered += string[i];
     } else if (string[i] === string[i].toUpperCase()) {
-      if (i == string.length) {
-        // let reducer = string.length - 1 - i;
-        // let modulo = key - reducer;
-
-        let upper = upperCase.findIndex((char) => char == string[i] + key);
-        ciphered += upperCase[upper + key];
-      } else if (i + key > string.length) {
+      if (i + key > string.length) {
         let reducer = string.length - i;
         let modulo = key - reducer;
 
@@ -24,13 +18,7 @@ function caesarCipher(string, key) {
         ciphered += upperCase[upper + key];
       }
     } else if (string[i] === string[i].toLowerCase()) {
-      if (i == string.length) {
-        // let reducer = string.length - 1 - i;
-        // let modulo = key - reducer;
-
-        let upper = lowerCase.findIndex((char) => char == string[i] + key);
-        ciphered += lowerCase[upper + key];
-      } else if (i + key > string.length) {
+      if (i + key > string.length) {
         let reducer = string.length - i;
         let modulo = key - reducer;
 
