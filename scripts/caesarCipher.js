0e799c1ce267986c0bd2b950901e3d116b4aba65
stvdeshon <stvdeshon@gmail.com>
2023-01-10 11:51:26 -0700

    Update Caesar Cipher

--- scripts/caesarCipher.js
+++ scripts/caesarCipher.js
@@ -1,5 +1,21 @@
-function caesarCipher(string) {
-  //shifted string
+function caesarCipher(string, key) {
+  let ciphered = "";
+  const lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
+  const upperCase = "ABCDEFGHIJKLMNOPQRSTUVXWYZ".split("");
+
+  for (let i in string) {
+    if (string[i] === " ") {
+      ciphered += string[i];
+    } else if (string[i] === string[i].toUpperCase()) {
+      const upper = upperCase.findIndex((char) => char == string[i]);
+      ciphered += upperCase[upper + key];
+    } else if (string[i] === string[i].toLowerCase()) {
+      const lower = lowerCase.findIndex((char) => char == string[i]);
+      ciphered += lowerCase[lower + key];
+    }
+  }
+
+  return ciphered;
 }
 
 module.exports = caesarCipher;
--- tests/caesarCipher.test.js
+++ tests/caesarCipher.test.js
@@ -1,5 +1,13 @@
 const caesarCipher = require("../scripts/caesarCipher");
 
-test("shifts every letter in a string to the next one in the alphabet", () => {
-  expect(caesarCipher("Julius")).toBe("Kvmjvt");
+test("shifts every letter in a string by one while retaining case", () => {
+  expect(caesarCipher("Julius", 1)).toBe("Kvmjvt");
+});
+
+test("works with spaces", () => {
+  expect(caesarCipher("Ab cD", 1)).toBe("Bc dE");
+});
+
+test("wraps from z to a", () => {
+  expect(caesarCipher("z", 2)).toBe("b");
 });
